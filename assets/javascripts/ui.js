7096714ce9e3ab822eca3669c64b5ca9d7711750
egardner <gardner.ec@gmail.com>
2016-05-19 14:28:00 -0700

    Automated commit at 2016-05-19 21:27:58 UTC by middleman-deploy 2.0.0-alpha

---
+++ assets/javascripts/ui.js
@@ -0,0 +1,38 @@
+function offCanvasNav() {
+  var $sidebar = $(".nav-sidebar");
+  var $menuButton = $("#navbar-menu");
+  var $closeButton = $("#nav-menu-close");
+  var $curtain = $(".sliding-panel-fade-screen");
+
+  $menuButton.on("click", function() {
+    $sidebar.toggleClass("visible");
+    $curtain.toggleClass("visible");
+    // Force css repaint to deal with webkit "losing" the menu contents
+    // on mobile devices
+    $('<style></style>').appendTo($(document.body)).remove();
+  });
+
+  $closeButton.on("click", function() {
+    $sidebar.removeClass("visible");
+    $curtain.removeClass("visible");
+  });
+
+  $curtain.on("click", function() {
+    $sidebar.removeClass("visible");
+    $curtain.removeClass("visible");
+  });
+
+  // bind escape key to menu close if menu is open
+  $(document).keydown(function(event) {
+    if (event.which === 27 && $sidebar.hasClass("visible")) {
+      $sidebar.removeClass("visible");
+      $curtain.removeClass("visible");
+    }
+  });
+}
+
+// Call the other setup functions inside of this setup function
+// Call this function inside of $(document).ready();
+function uiSetup() {
+  offCanvasNav();
+}
